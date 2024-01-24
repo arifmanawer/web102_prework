ba3297b4e1ef85895dec669c3a298dfec1449ada
arifmanawer <112596030+arifmanawer@users.noreply.github.com>
2024-01-23 22:15:55 -0500

    finished all challenges

--- index.js
+++ index.js
@@ -181,7 +181,16 @@ const sortedGames =  GAMES_JSON.sort( (item1, item2) => {
 });
 
 // use destructuring and the spread operator to grab the first and second games
+let [mostFunded, secondMostFunded, ...others] = sortedGames;
 
 // create a new element to hold the name of the top pledge game, then append it to the correct element
+const mostfundedpara = document.createElement("p");
+const mostfundednode = document.createTextNode(mostFunded.name);
+mostfundedpara.appendChild(mostfundednode);
+firstGameContainer.appendChild(mostfundedpara);
 
 // do the same for the runner up item
+const secondfundedpara = document.createElement("p");
+const secondfundednode = document.createTextNode(secondMostFunded.name);
+secondfundedpara.appendChild(secondfundednode);
+secondGameContainer.appendChild(secondfundedpara);
